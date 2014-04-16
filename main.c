284f96450443432ff3d0f169c7a608a21171dcef
Drew DeVault <sir@cmpwn.com>
2014-04-16 11:39:04 -0600

    Format existing filesystem

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <strings.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #define PAGE_LENGTH 0x4000
 
@@ -12,6 +15,8 @@ struct {
 	uint8_t dat_start;
 	FILE *rom;
 } context;
+
+uint32_t fatptr, datptr;
 
 void parse_context(int argc, char **argv) {
 	context.rom_file = context.model_dir = NULL;
@@ -40,15 +45,38 @@ void parse_context(int argc, char **argv) {
 		fprintf(stderr, errorMessage);
 		exit(1);
 	}
-	context.rom = fopen(context.rom_file, "rw");
+	context.rom = fopen(context.rom_file, "r+");
+	if (context.rom == NULL) {
+		fprintf(stderr, "Unable to open %s.\n", context.rom_file);
+		exit(1);
+	}
 	fseek(context.rom, 0L, SEEK_END);
 	uint32_t length = ftell(context.rom);
 	fseek(context.rom, 0L, SEEK_SET);
+
 	context.dat_start = 0x04;
-	context.fat_start = length / 0x4000 - 0x9;
+	context.fat_start = length / PAGE_LENGTH - 0x9;
+	fatptr = context.fat_start * PAGE_LENGTH;
+	datptr = (context.dat_start + 1) * PAGE_LENGTH - 1;
 }
 
 int main(int argc, char **argv) {
 	parse_context(argc, argv);
+	DIR *model = opendir(context.model_dir);
+	if (model == NULL) {
+		fprintf(stderr, "Unable to open %s.\n", context.model_dir);
+		fclose(context.rom);
+		exit(1);
+	}
+	uint8_t p;
+	uint8_t *blank_page = malloc(PAGE_LENGTH);
+	memset(blank_page, 0xFF, PAGE_LENGTH);
+	fseek(context.rom, context.dat_start * PAGE_LENGTH, SEEK_SET);
+	for (p = context.dat_start; p <= context.fat_start; ++p) {
+		fwrite(blank_page, PAGE_LENGTH, 1, context.rom);
+	}
+	fflush(context.rom);
+	fclose(context.rom);
+	free(blank_page);
 	return 0;
 }
